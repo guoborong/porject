52b49cd289daf6033ad26bd557c03d4d87808ab6
caolingxiong <caolingxiong@163.com>
2019-08-07 14:42:12 +0800

    修复项目不能运行的错误

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,3 @@
 module.exports = {
-	 lintOnSave: false,
-	devServer: {
-		proxy: {
-			'/api': {
-				target: 'http://192.168.1.188:12/api', //对应自己的接口
-				changeOrigin: true,
-				ws: true,
-				pathRewrite: {
-					'^/api': ''
-				}
-			}
-		}
-	}
+  lintOnSave: false
 }
